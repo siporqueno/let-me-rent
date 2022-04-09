3ed3427ef422bdc38f781e86b2d3dea72558209b
Artem <smirnov.artem.sam@gmail.com>
2022-04-09 19:42:26 +0400

    Initial commit

---
+++ source/core/src/main/java/ru/letmerent/core/LetMeRentApplication.java
@@ -0,0 +1,12 @@
+package ru.letmerent.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetMeRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetMeRentApplication.class, args);
+    }
+}
