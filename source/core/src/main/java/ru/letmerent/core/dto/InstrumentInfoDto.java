cfc34325211e465fadb9ea3a00d46d38b4a8cef5
Оськин Денис <oskin@iteco-inno.ru>
2022-04-16 10:13:35 +0300

    Подправление dto для проекта
    	Внгесли изменения в dto
    
    LMR-15

---
+++ source/core/src/main/java/ru/letmerent/core/dto/InstrumentInfoDto.java
@@ -0,0 +1,41 @@
+package ru.letmerent.core.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Schema(description = "Информация о модели инструмента")
+@ToString
+public class InstrumentInfoDto extends InstrumentDto {
+
+    @Schema(description = "Список URL-ссылок на изображения")
+    List<String> picturesUrls;
+
+    @Schema(description = "Имя владельца", example = "Александр")
+    String firstName;
+
+    @Schema(description = "Отчество владельца", example = "Родионович")
+    String secondName;
+
+    @Schema(description = "Фамилия владельца", example = "Бородач")
+    String lastName;
+
+    @Schema(description = "Электронная почта", example = "super_boroda@gmail.com")
+    String email;
+
+    @Schema(description = "Краткое описание инструмента", example = "Супер инструмент, одной рукой держу, другой слёзы счастья вытираю")
+    String description;
+
+    @Schema(description = "Интервалы занятости")
+    List<IntervalDto> intervals;
+}
---
+++ source/core/src/main/java/ru/letmerent/core/dto/IntervalDto.java
@@ -0,0 +1,26 @@
+package ru.letmerent.core.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Schema(description = "Модель интервала времени")
+@ToString
+public class IntervalDto {
+
+    @Schema(description = "Время старта аренды", example = "2015-11-21T12:48:00.973")
+    LocalDateTime dateStart;
+
+    @Schema(description = "Время завершения аренды", example = "2015-11-21T12:48:00.973")
+    LocalDateTime dateFinish;
+}
