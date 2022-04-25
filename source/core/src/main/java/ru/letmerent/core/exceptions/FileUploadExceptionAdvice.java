5ec1d0ff62dfaf74eb5d34deb50190194b69beb5
framzik <framzik@gmail.com!>
2022-04-25 22:16:47 +0300

    LMR-5 правка замечаний

--- source/core/src/main/java/ru/letmerent/core/exceptions/FileUploadExceptionAdvice.java
+++ source/core/src/main/java/ru/letmerent/core/exceptions/FileUploadExceptionAdvice.java
@@ -1,17 +1,23 @@
 package ru.letmerent.core.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ru.letmerent.core.exceptions.models.ApplicationError;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
     
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<Object> handleMaxSizeException(MaxUploadSizeExceededException exc) {
-        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("File too large!");
+    public ApplicationError handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        return ApplicationError.builder()
+            .errorCode(HttpStatus.BAD_REQUEST.value())
+            .userMessage("File too large!")
+            .date(new Date())
+            .build();
     }
 }
