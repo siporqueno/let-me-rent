3e12670c857fcb6f941a11def6610653ddca1184
unknown <strike1000001@gmail.com>
2022-04-30 18:48:43 +0300

    Forgot to add tools-list.js to previous commit.

--- source/core/src/main/resources/static/tools-list/tools-list.js
+++ source/core/src/main/resources/static/tools-list/tools-list.js
@@ -1,4 +1,4 @@
-angular.module('tools').controller('toolsListController', function ($scope, $http, $location,$routeParams, $localStorage) {
+angular.module('tools').controller('toolsListController', function ($scope, $http, $location, $routeParams, $localStorage) {
     const contextPath = 'http://localhost:8890/let-me-rent/api/v1/instruments';
     let currentPageIndex = 1;
 
@@ -24,10 +24,6 @@ angular.module('tools').controller('toolsListController', function ($scope, $htt
     };
 
 
-
-
-
-
     $scope.generatePagesIndexes = function (startPage, endPage) {
         let arr = [];
         for (let i = startPage; i < endPage + 1; i++) {
@@ -36,24 +32,24 @@ angular.module('tools').controller('toolsListController', function ($scope, $htt
         return arr;
     }
 
-    $(document).ready(function() {
-       $('.datepickerStart').datepicker({
-          format: 'dd-mm-yyyy'
+    $(document).ready(function () {
+        $('.datepickerStart').datepicker({
+            format: 'dd-mm-yyyy'
         }).datepicker("setDate", new Date());
     });
 
-    Date.prototype.addDays = function(days) {
-    this.setDate(this.getDate() + days);
-    return this;
+    Date.prototype.addDays = function (days) {
+        this.setDate(this.getDate() + days);
+        return this;
     };
 
-    $(document).ready(function() {
-    var currentDate = new Date();
-    var myDate = currentDate.addDays(28);
-           $('.datepickerEnd').datepicker({
-              format: 'dd-mm-yyyy'
-            }).datepicker("setDate", myDate);
-        });
+    $(document).ready(function () {
+        var currentDate = new Date();
+        var myDate = currentDate.addDays(28);
+        $('.datepickerEnd').datepicker({
+            format: 'dd-mm-yyyy'
+        }).datepicker("setDate", myDate);
+    });
 
     $scope.navToToolInfoPage = function (toolId) {
         $location.path('/tool-info/' + toolId);
@@ -61,17 +57,22 @@ angular.module('tools').controller('toolsListController', function ($scope, $htt
 
 
     $scope.tryToRent = function (toolId) {
-    //    ТУТ ЕСЛИ НЕ АВТОРИЗОВАН, ТО АЛЕРТ, ЧТО НАДО АВТОРИЗОВАТЬСЯ СНАЧАЛА, И ПЕРЕБРОСКА НА
-    // СТРАНИЦУ АВТОРИЗАЦИИ
-    // $location.path('/authorisation');
-    // А после авторизации/если авторизован, то уже формируем запрос на аренду на отдельной странице:
-    $location.path('/rent-request-page/' + toolId);
+        //    ТУТ ЕСЛИ НЕ АВТОРИЗОВАН, ТО АЛЕРТ, ЧТО НАДО АВТОРИЗОВАТЬСЯ СНАЧАЛА, И ПЕРЕБРОСКА НА
+        // СТРАНИЦУ АВТОРИЗАЦИИ
+        // $location.path('/authorisation');
+        // А после авторизации/если авторизован, то уже формируем запрос на аренду на отдельной странице:
+        $location.path('/rent-request-page/' + toolId);
     }
 
     $scope.putIntoCart = function (toolId) {
         // доработать запрос по энд-поинтам после добавления логики на бэке по Redis и авторизации
+        if (!$scope.filter.startDate || !$scope.filter.endDate) {
+            alert("Введите даты начала и окончания аренды");
+            return
+        }
         $http({
-            url: contextPath + 'api/v1/cart/' + $localStorage.webMarketGuestCartId + '/add/' + toolId,
+            url: contextPath + 'api/v1/cart/' + $localStorage.webMarketGuestCartId + '/add/'
+                + toolId + '/' + $scope.filter.startDate + '/' + $scope.filter.endDate,
             method: 'GET'
         }).then(function (response) {
             // $location.path('/cart/'); //опционально. Я бы не стала здесь сразу перебрасывать в корзину.
