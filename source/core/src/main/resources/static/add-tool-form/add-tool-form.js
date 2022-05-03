f3bd4c50d9c0a515fd902a6e713cb2bafee24e84
unknown <strike1000001@gmail.com>
2022-05-03 16:19:55 +0300

    Amended add-tool-form.html and add-tool-form.js

--- source/core/src/main/resources/static/add-tool-form/add-tool-form.js
+++ source/core/src/main/resources/static/add-tool-form/add-tool-form.js
@@ -1,16 +1,16 @@
-angular.module('tools').controller('addToolFormController', function ($scope, $http, $routeParams, $location) {
-    // const contextPath = 'http://localhost:8890/let-me-rent/api/v1/instruments';
-    //
-    // $scope.addTool = function () {
-    //     $http.put(contextPath + 'НАШ ЭНД ПОИНТ', $scope.new_tool)
-    //         .then(function successCallback (response) {
-    //             $scope.new_tool = null;
-    //             alert('Инструмент успешно добавлен в базу');
-    //             $location.path('/tools-list');
-    //         }, function failureCallback (response) {
-    //             alert(response.data.messages);
-    //         });
-    // }
-    //
+angular.module('tools').controller('addToolFormController', function ($scope, $http, $routeParams, $location, $localStorage) {
+    const contextPath = 'http://localhost:8890/let-me-rent/api/v1/instruments';
+
+    $scope.addTool = function () {
+        $scope.new_tool.ownerUsername = $localStorage.letMeRentUser.username;
+        $http.put(contextPath, $scope.new_tool)
+            .then(function successCallback(response) {
+                $scope.new_tool = null;
+                alert('Инструмент успешно добавлен в базу');
+                $location.path('/tools-list');
+            }, function failureCallback(response) {
+                alert(response.data.messages);
+            });
+    }
 
 });
