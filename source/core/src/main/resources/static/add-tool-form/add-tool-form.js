6b7dfa0a167a553d404491d86755b75103f5a120
unknown <strike1000001@gmail.com>
2022-05-05 18:48:05 +0300

    Inserted input field for category.

--- source/core/src/main/resources/static/add-tool-form/add-tool-form.js
+++ source/core/src/main/resources/static/add-tool-form/add-tool-form.js
@@ -2,8 +2,8 @@ angular.module('tools').controller('addToolFormController', function ($scope, $h
     const contextPath = 'http://localhost:8890/let-me-rent/api/v1/instruments';
 
     $scope.addTool = function () {
-        $scope.new_tool.ownerUsername = $localStorage.letMeRentUser.username;
-        $http.put(contextPath, $scope.new_tool)
+        // $scope.new_tool.ownerUsername = $localStorage.letMeRentUser.username;
+        $http.post(contextPath, $scope.new_tool)
             .then(function successCallback(response) {
                 $scope.new_tool = null;
                 alert('Инструмент успешно добавлен в базу');
