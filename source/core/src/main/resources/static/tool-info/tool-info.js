c44842bdbc174a9e8f6dc3f220e85caa6e1f4540
Daria-pugacheva <dpugacheva@yandex.ru>
2022-05-13 22:08:37 +0300

    Сделан вывод на страницу инструмента комментариев и рейтинга об инструменте  и о его владельце. Что касается коммента о владельце, то для меня актуальность на сегодня этих данных под вопросом, потому что мы пока в логике комментариев не предусматривали разделение - комментарий это о владельце или об арендаторе. Комментарий идет просто о пользователе, т.е. сюда по запросу прилетят все отзывы, в том числе о том, как человек пользовался какими-то инструментами. (В теории, ничего страшного, просто будет обширная картина, но может быть и такой косяк, что чел как владелец жлоб, а как пользователь золотой.)

--- source/core/src/main/resources/static/tool-info/tool-info.js
+++ source/core/src/main/resources/static/tool-info/tool-info.js
@@ -1,13 +1,12 @@
 angular.module('tools').controller('toolInfoController', function ($scope, $http, $routeParams, $location) {
-    const contextPath = 'http://localhost:8890/let-me-rent/api/v1/instruments/';
+    const contextPath = 'http://localhost:8890/let-me-rent';
 
     $scope.showToolInfo = function () {
-        $http.get(contextPath + $routeParams.toolId)
+        $http.get(contextPath + '/api/v1/instruments/'+ $routeParams.toolId)
             .then(function successCallback (response) {
                 $scope.tool = response.data;
             }, function failureCallback (response) {
-                // alert(response.data.messages); // это добавим, когда ошибки начнем с бэка передавать
-                alert("Пресловутое 'Что-то пошло не так :('") //это пока,потом уберем
+                alert(response.data.messages);
                 $location.path('/tools-list');
             });
     }
@@ -19,6 +18,86 @@ angular.module('tools').controller('toolInfoController', function ($scope, $http
 
     $scope.navToToolHistory = function (toolId) {
             $location.path('/tool-history/' + toolId);
+    }
+
+    $scope.toolComments = function (){
+        $http({
+            url: contextPath + '/api/v1/comments',
+            method: 'GET',
+            headers: {
+                "about": "instrument"
+            },
+            params: {
+                id: $routeParams.toolId
+            }
+        }).then(function successCallback(response) {
+            $scope.tool_comments = response.data;
+        }, function failureCallback(response) {
+            alert(response.data.messages);
+        });
+    }
+
+    $scope.toolGrade = function (){
+        $http({
+            url: contextPath + '/api/v1/comments/grade',
+            method: 'GET',
+            headers: {
+                "about": "instrument"
+            },
+            params: {
+                id: $routeParams.toolId
+            }
+        }).then(function successCallback(response) {
+            $scope.tool_grade = response.data;
+        }, function failureCallback(response) {
+            alert(response.data.messages);
+        });
+    }
+
+    $scope.showToolComments = function () {
+        $scope.toolComments();
+        $scope.toolGrade();
+        $scope.ascToolGrade = true;
+    }
+
+    $scope.ownerComments = function (){
+        $http({
+            url: contextPath + '/api/v1/comments',
+            method: 'GET',
+            headers: {
+                "about": "user"
+            },
+            params: {
+                id: $scope.tool.ownerId
+            }
+        }).then(function successCallback(response) {
+            $scope.owner_comments = response.data;
+        }, function failureCallback(response) {
+            alert(response.data.messages);
+        });
+    }
+
+    $scope.ownerGrade = function (){
+        $http({
+            url: contextPath + '/api/v1/comments/grade',
+            method: 'GET',
+            headers: {
+                "about": "user"
+            },
+            params: {
+                id: $scope.tool.ownerId
+            }
+        }).then(function successCallback(response) {
+            $scope.owner_grade = response.data;
+        }, function failureCallback(response) {
+            alert(response.data.messages);
+        });
+    }
+
+    $scope.showOwnerComments = function () {
+        $scope.ownerComments();
+        $scope.ownerGrade();
+        $scope.ascOwnerGrade = true;
     }
 
     $scope.showToolInfo();
