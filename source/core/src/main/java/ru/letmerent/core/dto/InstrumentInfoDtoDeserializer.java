c2a6721e4e628726cbbf98c5276f5ffa3999e7c6
unknown <strike1000001@gmail.com>
2022-05-19 14:46:23 +0300

    Added InstrumentInfoDeserializer to solve the issue with deserialization.

--- source/core/src/main/java/ru/letmerent/core/dto/InstrumentInfoDtoDeserializer.java
+++ source/core/src/main/java/ru/letmerent/core/dto/InstrumentInfoDtoDeserializer.java
@@ -9,6 +9,9 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class InstrumentInfoDtoDeserializer extends StdDeserializer<InstrumentInfoDto> {
 
@@ -23,41 +26,60 @@ public class InstrumentInfoDtoDeserializer extends StdDeserializer<InstrumentInf
     @Override
     public InstrumentInfoDto deserialize(JsonParser jp, DeserializationContext deserializationContext) throws IOException, JacksonException {
 
-//        JsonNode node = jp.getCodec().readTree(jp);
-//        Long id = node.get("id").longValue();
-//        LocalDateTime startDate = convertStringDateToLocalDateTime(node.get("startDate").toString().replace("\"",""));
-//        LocalDateTime endDate = convertStringDateToLocalDateTime(node.get("endDate").toString().replace("\"",""));
-//        BigDecimal rentPrice = node.get("rentPrice").decimalValue();
-//        Long rentLength = node.get("rentLength").longValue();
-//        JsonNode nodeInstrument = node.get("instrument");
-//        Long instrumentId = nodeInstrument.get("id").longValue();
-//        String title = nodeInstrument.get("title").toString().replace("\"","");
-//        String brandName = nodeInstrument.get("brandName").toString().replace("\"","");
-//        BigDecimal fee = nodeInstrument.get("fee").decimalValue();
-//        BigDecimal price = nodeInstrument.get("price").decimalValue();
-//        String ownerUsername = nodeInstrument.get("ownerUsername").toString().replace("\"","");
-//        String categoryName = nodeInstrument.get("categoryName").toString().replace("\"","");
-//        String avatarPictureUrl=nodeInstrument.get("avatarPictureUrl").toString().replace("\"","");
-//
-//        InstrumentForListDto instrument = new InstrumentForListDto();
-//        instrument.setId(instrumentId);
-//        instrument.setTitle(title);
-//        instrument.setBrandName(brandName);
-//        instrument.setFee(fee);
-//        instrument.setPrice(price);
-//        instrument.setOwnerUsername(ownerUsername);
-//        instrument.setCategoryName(categoryName);
-//        instrument.setAvatarPictureUrl(avatarPictureUrl);
-//
-//        OrderItemDto oi = new OrderItemDto();
-//        oi.setId(id);
-//        oi.setStartDate(startDate);
-//        oi.setEndDate(endDate);
-//        oi.setRentPrice(rentPrice);
-//        oi.setRentLength(rentLength);
-//        oi.setInstrument(instrument);
+        JsonNode node = jp.getCodec().readTree(jp);
+        Long id = node.get("id").longValue();
+        System.out.println(id);
+        String title = node.get("title").toString().replace("\"", "");
+        System.out.println(title);
+        String brandName = node.get("brandName").toString().replace("\"", "");
+        System.out.println(brandName);
+        BigDecimal fee = node.get("fee").decimalValue();
+        System.out.println(fee);
+        BigDecimal price = node.get("price").decimalValue();
+        System.out.println(price);
+        String ownerUsername = node.get("ownerUsername").toString().replace("\"", "");
+        System.out.println(ownerUsername);
+        String categoryName = node.get("categoryName").toString().replace("\"", "");
+        System.out.println(categoryName);
+        Long ownerId = node.get("ownerId").longValue();
+        System.out.println(ownerId);
+        String ownerFirstName = node.get("ownerFirstName").toString().replace("\"", "");
+        System.out.println(ownerFirstName);
+        String ownerSecondName = node.get("ownerSecondName").toString().replace("\"", "");
+        System.out.println(ownerSecondName);
+        String ownerLastName = node.get("ownerLastName").toString().replace("\"", "");
+        System.out.println(ownerLastName);
+        String ownerEmail = node.get("ownerEmail").toString().replace("\"", "");
+        System.out.println(ownerEmail);
+        String description = node.get("description").toString().replace("\"", "");
+        System.out.println(description);
+        List<IntervalDto> intervals = new ArrayList<>();
+        Collection<String> pictureUrls = new ArrayList<>();
+        LocalDateTime startDate = convertStringDateToLocalDateTime(node.get("startDate").toString().replace("\"", ""));
+        System.out.println(startDate);
+        LocalDateTime endDate = convertStringDateToLocalDateTime(node.get("endDate").toString().replace("\"", ""));
+        System.out.println(endDate);
 
-        return new InstrumentInfoDto();
+        InstrumentInfoDto instrum = new InstrumentInfoDto();
+        instrum.setId(id);
+        instrum.setTitle(title);
+        instrum.setBrandName(brandName);
+        instrum.setPrice(price);
+        instrum.setFee(fee);
+        instrum.setOwnerUsername(ownerUsername);
+        instrum.setCategoryName(categoryName);
+        instrum.setOwnerId(ownerId);
+        instrum.setOwnerFirstName(ownerFirstName);
+        instrum.setOwnerSecondName(ownerSecondName);
+        instrum.setOwnerLastName(ownerLastName);
+        instrum.setOwnerEmail(ownerEmail);
+        instrum.setDescription(description);
+        instrum.setPictureUrls(pictureUrls);
+        instrum.setIntervals(intervals);
+        instrum.setStartDate(startDate);
+        instrum.setEndDate(endDate);
+
+        return instrum;
     }
 
     private LocalDateTime convertStringDateToLocalDateTime(String date) {
