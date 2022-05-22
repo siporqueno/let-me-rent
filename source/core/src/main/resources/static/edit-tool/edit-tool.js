eea250ae4224c5bd048ef1acd9d93e2a709b70fb
unknown <strike1000001@gmail.com>
2022-05-22 21:38:27 +0300

    Realized pictures upload during edit of instrument and check for lack of pictures to upload for new and edited instruments.

--- source/core/src/main/resources/static/edit-tool/edit-tool.js
+++ source/core/src/main/resources/static/edit-tool/edit-tool.js
@@ -1,44 +1,84 @@
-angular.module('tools').controller('editToolController', function ($scope, $http, $routeParams, $location) {
-    const contextPath = 'http://localhost:8890/let-me-rent/api/v1/instruments';
+angular
+    .module('tools')
+    .controller('editToolController', function ($scope, $http, $routeParams, $location) {
+        const contextPath = 'http://localhost:8890/let-me-rent';
 
-    $scope.prepareToolForUpdate = function () {
-        $http.get(contextPath + '/' + $routeParams.toolId)
-            .then(function successCallback(response) {
-                $scope.updated_tool = response.data;
-                console.log($scope.updated_tool);
-                // $scope.earliestPossibleEndDate = response.data.lastRentDate;
-            }, function failureCallback(response) {
-                console.log(response);
-                alert(response.data.messages);
-                $location.path('/profile');
-            });
-    }
-
-    $scope.updateTool = function () {
-        console.log('Last rent date: ' + $scope.updated_tool.lastRentDate);
-        if ($scope.updated_tool.endDate < $scope.updated_tool.lastRentDate) {
-            alert("Предоставление инструмента в аренду не может быть приостановлено раньше окончания последнего забронированного периода аренды");
-        } else {
-            console.log($scope.updated_tool);
-            $http.put(contextPath, $scope.updated_tool)
+        $scope.prepareToolForUpdate = function () {
+            $http.get(contextPath + '/api/v1/instruments/' + $routeParams.toolId)
                 .then(function successCallback(response) {
-                    $scope.updated_tool = null;
-                    alert('Продукт успешно обновлен');
-                    $location.path('/profile');
+                    $scope.updated_tool = response.data;
+                    console.log($scope.updated_tool);
                 }, function failureCallback(response) {
+                    console.log(response);
                     alert(response.data.messages);
+                    $location.path('/profile');
                 });
         }
 
-    }
+        $scope.updateTool = function () {
+            console.log('Last rent date: ' + $scope.updated_tool.lastRentDate);
+            if ($scope.updated_tool.endDate < $scope.updated_tool.lastRentDate) {
+                alert("Предоставление инструмента в аренду не может быть приостановлено раньше окончания последнего забронированного периода аренды");
+            } else {
+                console.log($scope.updated_tool);
+                $http.put(contextPath + '/api/v1/instruments', $scope.updated_tool)
+                    .then(function successCallback(response) {
+                        let instrumentId = $scope.updated_tool.id;
+                        $scope.updated_tool = null;
+                        alert('Продукт без добавления фото успешно обновлен');
 
-    $scope.removePicture = function (url, pictureUrls) {
-        console.log('Before: ' + $scope.updated_tool.pictureUrls);
-        pictureUrls.splice(pictureUrls.indexOf(url), 1);
-        console.log('Deleted picture');
-        console.log('After: ' + $scope.updated_tool.pictureUrls);
-    }
+                        if ($scope.pictures) {
 
-    $scope.prepareToolForUpdate();
+                            console.log($scope.pictures);
+                            let formData = new FormData();
+                            for (let i = 0; i < $scope.pictures.length; i++) {
+                                formData.append('pictures', $scope.pictures[i]);
+                                console.log('File name: ' + $scope.pictures[i].name + ', size: ' + $scope.pictures[i].size);
+                            }
+                            formData.append('instrumentId', instrumentId);
+                            $http({
+                                url: contextPath + "/api/v1/pictures/uploads",
+                                method: 'POST',
+                                headers: {'Content-Type': undefined},
+                                data: formData
+                            }).then(function successCallback() {
+                                console.log('Новые фото успешно добавлены');
+                            }, function failureCallback(response) {
+                                alert(response.data.messages);
+                            });
 
-});
+                        }
+
+                        $location.path('/profile');
+                    }, function failureCallback(response) {
+                        alert(response.data.messages);
+                    });
+            }
+
+        }
+
+        $scope.removePicture = function (url, pictureUrls) {
+            console.log('Before: ' + $scope.updated_tool.pictureUrls);
+            pictureUrls.splice(pictureUrls.indexOf(url), 1);
+            console.log('Deleted picture');
+            console.log('After: ' + $scope.updated_tool.pictureUrls);
+        }
+
+        $scope.prepareToolForUpdate();
+
+    })
+    .directive('filesModel', ['$parse', function ($parse) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var model = $parse(attrs.filesModel);
+                var modelSetter = model.assign;
+
+                element.bind('change', function () {
+                    scope.$apply(function () {
+                        modelSetter(scope, element[0].files);
+                    });
+                });
+            }
+        };
+    }]);
