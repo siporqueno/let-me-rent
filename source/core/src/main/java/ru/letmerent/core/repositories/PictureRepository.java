5418dc3a1a4504abeea9a323357bab04c854fde2
framzik <framzik@gmail.com!>
2022-05-23 21:00:19 +0300

    elудаление картинок при обновлении инструмента

--- source/core/src/main/java/ru/letmerent/core/repositories/PictureRepository.java
+++ source/core/src/main/java/ru/letmerent/core/repositories/PictureRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface PictureRepository extends JpaRepository<Picture, Long> {
     List<Picture> findAllByInstrumentId(Long instrumentId);
+    Picture findByName(String name);
 }
